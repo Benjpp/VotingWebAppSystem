cf03d5fdf2c027b1948639cfd40a41c79d7e2c7c
Benjpp <perezperezbenjamin61@gmail.com>
2024-10-21 20:12:49 +0200

    Corregido falta de paquete test

--- src/test/java/EntidadesHumanasTesting/VotanteTest.java
+++ src/test/java/EntidadesHumanasTesting/VotanteTest.java
@@ -1,5 +1,13 @@
 package EntidadesHumanasTesting;
 
-public class VotanteTest {
+import org.junit.jupiter.api.BeforeEach;
 
+import EntidadesHumanas.Votante;
+
+public class VotanteTest {
+	Votante vot;
+	
+	@BeforeEach
+	public void construirNuevoVotante() {
+	}
 }
